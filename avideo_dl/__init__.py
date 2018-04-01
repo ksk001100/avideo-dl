61c41f8cb752b18462e0ed2ef64043a87526365f
ksk001100 <hm.pudding0715@gmail.com>
2018-04-01 14:06:07 +0900

    add docstring

--- avideo_dl/__init__.py
+++ avideo_dl/__init__.py
@@ -6,8 +6,8 @@ from .AVideoDownloader import AVideoDownloader
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('url', help='URLの指定')
-    parser.add_argument('-t', '--thread', help='スレッド数を指定',
+    parser.add_argument('url', help='Specify URL')
+    parser.add_argument('-t', '--thread', help='Specify the number of threads',
                         type=int, default=10,
                         choices=[1, 10, 30, 50, 80, 100])
     args = parser.parse_args()
