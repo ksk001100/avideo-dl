12dea11a42aca2c007989e8b29e1512cf114f31e
KeisukeToyota <hm.pudding0715@gmai.com>
2019-04-03 15:29:38 +0900

    commit

--- avideo_dl/AVideoDownloader.py
+++ avideo_dl/AVideoDownloader.py
@@ -1,12 +1,13 @@
-import urllib.request
-import urllib.error
+import os
 import sys
-from threading import Thread
+import urllib.error
+import urllib.request
 from multiprocessing import cpu_count
 from queue import Queue
-import os
-from .extractor import URLExtractor
+from threading import Thread
+
 from .aa import ascii_moji
+from .extractor import URLExtractor
 
 
 class AVideoDownloader:
@@ -22,7 +23,8 @@ class AVideoDownloader:
         self.file_count = 0
         self.extractor = URLExtractor()
         self.video_url, self.title = self.extractor.get_url(url)
-        print('title : {}\nvideo_url : {}\n'.format(self.title, self.video_url))
+        print('title : {}\nvideo_url : {}\n'.format(self.title,
+                                                    self.video_url))
 
     def split_download(self, queue):
         """Split download
@@ -57,17 +59,23 @@ class AVideoDownloader:
                 progress = "=" * p_count
                 space = " " * (100 - p_count)
                 arrow = ">"
-                print("\r[{}{}{}]{}%".format(
-                    progress, arrow, space,
-                    int(self.file_count * (100 / self.split_num))), end='')
+                print(
+                    "\r[{}{}{}]{}%".format(
+                        progress, arrow, space,
+                        int(self.file_count * (100 / self.split_num))),
+                    end='')
             queue.task_done()
 
     def single_download(self):
         """Single download"""
+
         def progress(block_count, block_size, total_size):
             percentage = 100.0 * block_count * block_size / total_size
-            sys.stdout.write("%.2f %% ( %d KB )\r" % (percentage, total_size / 1024))
-        urllib.request.urlretrieve(self.video_url, "{}.mp4".format(self.title), progress)
+            sys.stdout.write(
+                "%.2f %% ( %d KB )\r" % (percentage, total_size / 1024))
+
+        urllib.request.urlretrieve(self.video_url, "{}.mp4".format(self.title),
+                                   progress)
 
     def download(self):
         """Download"""
@@ -93,7 +101,8 @@ class AVideoDownloader:
 
         queue = Queue()
         total_count = 0
-        for i, val in enumerate([(self.total_length + j) // self.split_num for j in range(self.split_num)]):
+        for i, val in enumerate([(self.total_length + j) // self.split_num
+                                 for j in range(self.split_num)]):
             if i == 0:
                 last = val
                 total_count = val
@@ -105,7 +114,7 @@ class AVideoDownloader:
 
         threads = []
         for _ in range(cpu_count()):
-            thread = Thread(target=self.split_download, args=(queue,))
+            thread = Thread(target=self.split_download, args=(queue, ))
             thread.start()
             threads.append(thread)
 
@@ -116,7 +125,10 @@ class AVideoDownloader:
             thread.join()
 
         self.combine(self.title)
-        return str(round(os.path.getsize(self.title + '.' + self.file_type) / (1024.0 ** 2), 1)) + 'MB'
+        return str(
+            round(
+                os.path.getsize(self.title + '.' + self.file_type) /
+                (1024.0**2), 1)) + 'MB'
 
     def combine(self, file_name):
         """Combine split file
