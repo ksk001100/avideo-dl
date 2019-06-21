79015e0412d32f9e846590c598af6576bca05924
KeisukeToyota <hm.pudding0715@gmai.com>
2019-06-21 12:22:46 +0900

    fix

--- avideo_dl/utils.py
+++ avideo_dl/utils.py
@@ -16,3 +16,11 @@ def snake_case(s: str) -> str:
 def service_name(url: str) -> str:
     pattern = ['www', 'com', 'co', 'jp', 'se', 'video']
     return next(filter(lambda x: x not in pattern, urlparse(url).netloc.split('.')))
+
+def progress_bar(count, split_num):
+    p_count = int(100 * count / split_num)
+    progress = "=" * p_count
+    space = " " * (100 - p_count)
+    arrow = ">"
+    print("\r[{}{}{}] {}% ({}/{})".format(progress, arrow, space, p_count,
+                                          count, split_num), end='')
