7315c94a0e089ac99eafc46a259bbad9fa9b4c76
ksk001100 <hm.pudding0715@gmail.com>
2019-07-22 05:01:44 +0900

    stabled pornhub

--- avideo_dl/extractor/pornhub.py
+++ avideo_dl/extractor/pornhub.py
@@ -1,6 +1,7 @@
 import urllib.request
 import re
 import os
+import http
 from datetime import datetime
 
 from avideo_dl.utils import headers
@@ -9,8 +10,14 @@ from avideo_dl.extractor.base import BaseExtractor
 class PornhubExtractor(BaseExtractor):
     @classmethod
     def get_video_url(cls, url):
-        request = urllib.request.Request(url=url, headers=headers())
-        html = urllib.request.urlopen(request).read().decode('utf-8')
+        while True:
+            try:
+                request = urllib.request.Request(url=url, headers=headers())
+                html = urllib.request.urlopen(request).read().decode('utf-8')
+            except http.client.IncompleteRead:
+                continue
+            else:
+                break
         title = next(iter(os.path.basename(__file__).split('.'))) + datetime.now().strftime('%Y%m%d%H%M%S')
         video_url = re.findall(
             r'(?<=videoUrl":").*?480P.*?(?=")', str(html))[0].replace('\\', '')
